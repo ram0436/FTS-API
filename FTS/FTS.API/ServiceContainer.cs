99db4ed01a4adea3853ac90848ffadb237aa61af
Ram <ram.0436@gmail.com>
2021-06-27 21:06:55 +0530

    Added controllers

--- FTS/FTS.API/ServiceContainer.cs
+++ FTS/FTS.API/ServiceContainer.cs
@@ -12,9 +12,46 @@ namespace FTS.API
         {
             // Service Container
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IBookService, BookService>();
+            services.AddScoped<ICommercialServiceService, CommercialServiceService>();
+            services.AddScoped<IElectricApplianceService, ElectricApplianceService>();
+
+            services.AddScoped<IFashionService, FashionService>();
+            services.AddScoped<IFurnitureService, FurnitureService>();
+            services.AddScoped<IGadgetService, GadgetService>();
+            services.AddScoped<IHobbyService, HobbyService>();
+
+            services.AddScoped<IHouseApartmentService, HouseApartmentService>();
+            services.AddScoped<IJobService, JobService>();
+            services.AddScoped<ILandPlotService, LandPlotService>();
+            services.AddScoped<IOfficeShopService, OfficeShopService>();
+
+            services.AddScoped<IPetService, PetService>();
+            services.AddScoped<IPGService, PGService>();
+            services.AddScoped<ISportService, SportService>();
+            services.AddScoped<IVehicleService, VehicleService>();
+
 
             //Repository Container
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddScoped<ICommercialServiceRepository, CommercialServiceRepository>();
+            services.AddScoped<IElectricApplianceRepository, ElectricApplianceRepository>();
+
+            services.AddScoped<IFashionRepository, FashionRepository>();
+            services.AddScoped<IFurnitureRepository, FurnitureRepository>();
+            services.AddScoped<IGadgetRepository, GadgetRepository>();
+            services.AddScoped<IHobbyRepository, HobbyRepository>();
+
+            services.AddScoped<IHouseApartmentRepository, HouseApartmentRepository>();
+            services.AddScoped<IJobRepository, JobRepository>();
+            services.AddScoped<ILandPlotRepository, LandPlotRepository>();
+            services.AddScoped<IOfficeShopRepository, OfficeShopRepository>();
+
+            services.AddScoped<IPetRepository, PetRepository>();
+            services.AddScoped<IPGRepository, PGRepository>();
+            services.AddScoped<ISportRepository, SportRepository>();
+            services.AddScoped<IVehicleRepository, VehicleRepository>();
 
         }
     }
